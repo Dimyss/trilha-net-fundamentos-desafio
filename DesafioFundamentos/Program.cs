d95857d84436aa273983c1701d4f93d5a4cd2b13
Dmitry <dimysilvaa7@gmail.com>
2024-09-21 14:43:32 -0300

    Alteração-desafio

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,10 +1,13 @@
 ﻿using DesafioFundamentos.Models;
+//Editado por: Dmitry!
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
+
+Console.Clear();
 
 Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
@@ -32,18 +35,22 @@ while (exibirMenu)
     switch (Console.ReadLine())
     {
         case "1":
+            Console.Clear();
             es.AdicionarVeiculo();
             break;
 
         case "2":
+            Console.Clear();
             es.RemoverVeiculo();
             break;
 
         case "3":
+            Console.Clear();
             es.ListarVeiculos();
             break;
 
         case "4":
+            Console.Clear();
             exibirMenu = false;
             break;
 
